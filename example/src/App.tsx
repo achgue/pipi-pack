0f5c957c62b40d42336c106f024949fca2c67f75
achgue <83014206+achgue@users.noreply.github.com>
2023-05-19 11:17:55 +0200

    roba bella

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'pipi-pack'
+import 'pipi-pack/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
